c65a021b9f02b6d03615e040f95b10256a4a458e
igrlk <iluchenkov@gmail.com>
2025-04-05 16:51:18 +0100

    playwright

---
+++ e2e/addon.spec.ts
@@ -0,0 +1,111 @@
+import { expect, test } from '@playwright/test';
+
+test('Submit multi-step form', async ({ page }) => {
+	await page.goto(
+		'http://localhost:6006/?path=/story/stories-multistepform--default',
+	);
+	await page.getByRole('tab', { name: 'Interaction Recorder' }).click();
+	await page
+		.getByRole('button', { name: 'Start recording Stop recording' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Name' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Name' })
+		.fill('john doe');
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Email' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Email' })
+		.fill('johndoe@gmail.com');
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('button', { name: 'Submit' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Phone' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Phone' })
+		.fill('12345');
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Address' })
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('textbox', { name: 'Address' })
+		.fill('42 avenue');
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByRole('button', { name: 'Submit' })
+		.click();
+	await page.getByRole('button', { name: 'Add assertion' }).click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('email: johndoe@gmail.com')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('to be visible')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('phone:')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('to be visible')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('phone:')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('to be visible')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('address: 42 avenue')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('to be visible')
+		.click();
+	await page
+		.locator('iframe[title="storybook-preview-iframe"]')
+		.contentFrame()
+		.getByText('name: john doe')
+		.click();
+
+	await page.waitForTimeout(1000);
+	const element = page.getByText('Play Functionplay: async ({');
+	expect(await element.textContent()).toMatchSnapshot();
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defaultExclude, defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		environment: 'happy-dom',
+		exclude: [...defaultExclude, 'e2e/**/*'],
 	},
 });
