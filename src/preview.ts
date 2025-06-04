e54060913c6d681846bc8be719378bbc3c91ec39
Rein Undheim <rein@undheim.io>
2025-06-04 18:25:18 +0200

    fix: fixes an issue where using getAbsolutePath would make the addon not work

--- src/preview.ts
+++ src/preview.ts
@@ -3,12 +3,11 @@ import type { ProjectAnnotations, Renderer } from 'storybook/internal/types';
 import { IS_ASSERTING_KEY, IS_RECORDING_KEY } from './constants';
 import { withInteractionRecorder } from './decorators/with-interaction-recorder';
 
-const preview: ProjectAnnotations<Renderer> = {
-	decorators: [withInteractionRecorder],
-	initialGlobals: {
-		[IS_RECORDING_KEY]: false,
-		[IS_ASSERTING_KEY]: false,
-	},
-};
+export const decorators: ProjectAnnotations<Renderer>['decorators'] = [
+	withInteractionRecorder,
+];
 
-export default preview;
+export const initialGlobals: ProjectAnnotations<Renderer>['initialGlobals'] = {
+	[IS_RECORDING_KEY]: false,
+	[IS_ASSERTING_KEY]: false,
+};
